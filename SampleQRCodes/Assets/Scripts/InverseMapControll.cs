816f43d5391c3feda78e5bf19a577d2ef7a97213
ErikToth97 <erik.toth97@gmail.com>
2023-09-11 12:05:24 +0200

    working inverse kinematics

--- SampleQRCodes/Assets/Scripts/InverseMapControll.cs
+++ SampleQRCodes/Assets/Scripts/InverseMapControll.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using InverseMap;
+using System;
 
 public class InverseMapControll : MonoBehaviour
 {
     // Start is called before the first frame update
     UR5 _inverse;
-    private static List<Transform> joints;
+    private List<Transform> joints;
+    public int branchInd = 0;
+    public double tcpdist = 10;
     public GameObject endEffector, robotArm;
     private Vector3 lastPos;
 
@@ -34,48 +37,68 @@ public class InverseMapControll : MonoBehaviour
         lastPos = new Vector3();
     }
 
-    public double[] GetInverse(double[] pose = null, double[] joints = null, int gripperId = -1)
+    public double[] GetInverse(double[] pose = null, double[] jointOffsets = null, int gripperId = -1)
     {
         var pos = endEffector.transform.position * 1000f;
         lastPos = pos;
         double[] full = new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        _inverse.SetInputVariable(new string[] { "Ox", "Oy", "Oz", "ax", "ay", "az", "bx", "by", "bz", "cx", "cy", "cz" }, 
-            new double[] { pos.x, pos.y, pos.z, 1.0, 0, 0, 0, 1.0, 0, 0, 0, 1.0 });
+        _inverse.SetInputVariable(new string[] { "Ox", "Oy", "Oz", "ax", "ay", "az", "bx", "by", "bz", "cx", "cy", "cz","tcpdist" }, 
+            new double[] { -pos.x, pos.z, pos.y, 1.0, 0, 0, 0, 1.0, 0, 0, 0, 1.0, tcpdist});
         _inverse.Calculate();
         var inverseBranches = _inverse.GetOutputVariables(new string[] { "q1", "q2", "q3", "q4", "q5", "q6" }, _inverse.BranchValid);
 
         if (inverseBranches.Length == 0)
             return new double[] { 0 };
 
-        return inverseBranches[0];
+        //return inverseBranches[0];
         //double[] actualJoints = joints;
-        //double[] bestJoints = new double[6];
-        //double bestJointRotSum = double.MaxValue;
+        double[] bestJoints = new double[6];
+
+        double bestJointRotSum = double.MaxValue;
+        List<int> validBranchInd = new List<int>();
+        for (int b = 0; b < inverseBranches.Length; ++b)
+        {
+            if (inverseBranches[b][1] > -100*Mathf.Deg2Rad && inverseBranches[b][1] < 100* Mathf.Deg2Rad)
+                validBranchInd.Add(b);
+        }
+        double[][] validBranches = new double[validBranchInd.Count][];
+        for (int i = 0; i < validBranchInd.Count; i++)
+        {
+            validBranches[i] = inverseBranches[validBranchInd[i]];
+        }
+        bestJoints = validBranches[0];
+        if (validBranchInd.Count > branchInd)
+            bestJoints = validBranches[branchInd];
         //for (int b = 0; b < inverseBranches.Length; ++b)
         //{
         //    double jointRotSum = 0, calculated, alternative;
         //    double[] branchBest = new double[6];
         //    for (int j = 0; j < inverseBranches[b].Length; ++j)
         //    {
-        //        //// Check +-360 rotations, in valid direction
-        //        //calculated = inverseBranches[b][j];
-        //        //branchBest[j] = calculated;
-        //        //if (Name.Contains("UR"))
-        //        //{
-        //        //    alternative = calculated < 0 ? calculated + (2 * Math.PI) : calculated - (2 * Math.PI);
-        //        //    if (Math.Abs(actualJoints[j] - calculated) > Math.Abs(actualJoints[j] - alternative))
-        //        //        branchBest[j] = alternative;
-        //        //}
-        //        //// Smallest distance to actual joints
-        //        //jointRotSum += Math.Abs(actualJoints[j] - branchBest[j]);
+        //        // Check +-360 rotations, in valid direction
+        //        calculated = inverseBranches[b][j];
+        //        branchBest[j] = calculated;
+        //        alternative = calculated < 0 ? calculated + (2 * Math.PI) : calculated - (2 * Math.PI);
+        //        if (Math.Abs(actualJoints[j] - calculated) > Math.Abs(actualJoints[j] - alternative))
+        //            branchBest[j] = alternative;
+        //        // Smallest distance to actual joints
+        //        jointRotSum += Math.Abs(actualJoints[j] - branchBest[j]);
         //    }
         //    if (jointRotSum < bestJointRotSum)
         //    {
         //        bestJointRotSum = jointRotSum;
         //        branchBest.CopyTo(bestJoints, 0);
         //    }
+
         //}
-        //return bestJoints;
+
+        double[] branchMultiply = new double[6] { 1, 1, 1, 1, -1, 1 };
+        for (int i = 0; i < 6; i++)
+        {
+            bestJoints[i] *= branchMultiply[i];
+            bestJoints[i] += jointOffsets[i];
+        }
+        return bestJoints;
     }
 
 
@@ -84,8 +107,8 @@ public class InverseMapControll : MonoBehaviour
     {
         var pos = endEffector.transform.position * 1000f;
         if (lastPos != pos)
-        {
-            double[] angles = GetInverse(null, new double[] { 0, -0.5 * Mathf.PI, 0, -0.5 * Mathf.PI, 0, 0 }, -1);
+        {   // 0 +20 -45
+            double[] angles = GetInverse(null, new double[] { -Math.PI/2, Math.PI / 2, 0, Math.PI, 0, 0 }, -1);
             if(angles.Length == 6)
             {
                 float[] anglesf = new float[6];
@@ -104,6 +127,11 @@ public class InverseMapControll : MonoBehaviour
         {
             //joints[i].Rotate(new Vector3(rotations[i * 3], rotations[i * 3 + 1], rotations[i * 3 + 2]), Space.Self);
             joints[i].localRotation = Quaternion.Euler(rotations[i] * rotAxis[i][0], rotations[i] * rotAxis[i][1], rotations[i] * rotAxis[i][2]);
+        }
+        if((joints[5].position - endEffector.transform.position).magnitude > 0.1+ tcpdist)
+        {
+            rotations[3] += 180;
+            joints[3].localRotation = Quaternion.Euler(rotations[3] * rotAxis[3][0], rotations[3] * rotAxis[3][1], rotations[3] * rotAxis[3][2]);
         }
     }
 
