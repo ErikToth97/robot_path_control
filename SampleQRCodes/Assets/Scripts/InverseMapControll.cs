08ef2ca7e6233cee396142926232f78c24a01bf7
ErikToth97 <erik.toth97@gmail.com>
2023-10-18 17:16:51 +0200

    working rotation in inverse kinematics

--- SampleQRCodes/Assets/Scripts/InverseMapControll.cs
+++ SampleQRCodes/Assets/Scripts/InverseMapControll.cs
@@ -10,9 +10,10 @@ public class InverseMapControll : MonoBehaviour
     UR5 _inverse;
     private List<Transform> joints;
     public int branchInd = 0;
-    public double tcpdist = 1000;
+    public double tcpdist = 100;
     public GameObject endEffector, robotArm;
     private Vector3 lastPos;
+    private Quaternion lastOri;
 
     private static float[] testangles;
     private static List<float[]> rotAxis;
@@ -35,6 +36,7 @@ public class InverseMapControll : MonoBehaviour
 
         _inverse = new UR5();
         lastPos = new Vector3();
+        lastOri = new Quaternion();
     }
 
     public double[] GetInverse(double[] pose = null, double[] jointOffsets = null, int gripperId = -1)
@@ -42,8 +44,15 @@ public class InverseMapControll : MonoBehaviour
         var pos = endEffector.transform.position * 1000f;
         lastPos = pos;
         double[] full = new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        var ori = endEffector.transform.rotation.eulerAngles;
+        var switchedAxis = new Vector3(ori.x, -ori.z, -ori.y);
+        Matrix4x4 rot = Matrix4x4.Rotate(Quaternion.Euler(switchedAxis));
         _inverse.SetInputVariable(new string[] { "Ox", "Oy", "Oz", "ax", "ay", "az", "bx", "by", "bz", "cx", "cy", "cz","tcpdist" }, 
-            new double[] { -pos.x, pos.z, pos.y, 1.0, 0, 0, 0, 1,0, 0, 0, 1, tcpdist});
+            new double[] { -pos.x, pos.z, pos.y,
+                            rot[0, 0], rot[0, 1], rot[0, 2], 
+                            rot[1, 0], rot[1, 1], rot[1, 2],
+                            rot[2, 0], rot[2, 1], rot[2, 2], 
+                            tcpdist});
         _inverse.Calculate();
         var inverseBranches = _inverse.GetOutputVariables(new string[] { "q1", "q2", "q3", "q4", "q5", "q6" }, _inverse.BranchValid);
 
@@ -66,6 +75,8 @@ public class InverseMapControll : MonoBehaviour
         {
             validBranches[i] = inverseBranches[validBranchInd[i]];
         }
+        if (validBranches.Length == 0)
+            return new double[] { 0 };
         bestJoints = validBranches[0];
         if (validBranchInd.Count > branchInd)
             bestJoints = validBranches[branchInd];
@@ -106,9 +117,11 @@ public class InverseMapControll : MonoBehaviour
     void Update()
     {
         var pos = endEffector.transform.position * 1000f;
-        if (lastPos != pos)
+        var ori = endEffector.transform.rotation;
+        if (lastPos != pos || lastOri != ori)
         {   // 0 +20 -45
-            double[] angles = GetInverse(null, new double[] { -Math.PI/2, Math.PI / 2, 0, Math.PI/2, Math.PI, 0 }, -1);
+            lastOri = ori;
+            double[] angles = GetInverse(null, new double[] { -Math.PI/2, Math.PI / 2, 0, Math.PI/2, 0, 0 }, -1);
             if(angles.Length == 6)
             {
                 float[] anglesf = new float[6];
@@ -128,10 +141,10 @@ public class InverseMapControll : MonoBehaviour
             //joints[i].Rotate(new Vector3(rotations[i * 3], rotations[i * 3 + 1], rotations[i * 3 + 2]), Space.Self);
             joints[i].localRotation = Quaternion.Euler(rotations[i] * rotAxis[i][0], rotations[i] * rotAxis[i][1], rotations[i] * rotAxis[i][2]);
         }
-        if((joints[5].position - endEffector.transform.position).magnitude > 0.1+ tcpdist)
+        if((joints[5].position - endEffector.transform.position).magnitude > 0.1+ tcpdist/1000)
         {
             rotations[3] += 180;
-            joints[3].localRotation = Quaternion.Euler(rotations[3] * rotAxis[3][0], rotations[3] * rotAxis[3][1], rotations[3] * rotAxis[3][2]);
+            joints[5].localRotation = Quaternion.Euler(rotations[5] * rotAxis[5][0], rotations[5] * rotAxis[5][1], rotations[5] * rotAxis[5][2]);
         }
     }
 
@@ -144,7 +157,7 @@ public class InverseMapControll : MonoBehaviour
             rotAxis[i++] = new float[3] { 0.0f, 0.0f, 1.0f };
             rotAxis[i++] = new float[3] { 0.0f, 0.0f, 1.0f };
             rotAxis[i++] = new float[3] { 0.0f, 0.0f, 1.0f };
-            rotAxis[i++] = new float[3] { 0.0f, 1.0f, 0.0f };
+            rotAxis[i++] = new float[3] { 0.0f, -1.0f, 0.0f };
             rotAxis[i++] = new float[3] { 0.0f, 0.0f, 1.0f };
         }
         else
